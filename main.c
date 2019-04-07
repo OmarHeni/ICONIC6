4dc6466d113f2522e10749326d60240db4f686c9
OmarHeni <47540862+OmarHeni@users.noreply.github.com>
2019-04-07 10:42:32 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ int rep = 0  ;
 int j,o ; 
 int ex= 0;
 int i;
+machine mach;
+enemie  enm;
 enigme en ; 
 sta.run=1;
 menu.j = -1;
@@ -24,29 +26,40 @@ intial(&en);
 i=5;
 SDL_Init(SDL_INIT_EVERYTHING);
 TTF_Init();
-action (&sta,&manj,&menu);
+action (&sta,&manj,&menu,&mach,&enm);
+printf ("part %d | %d \n",enm.posenm.y,enm.posenm.x);
 Mix_PlayMusic(sta.music,-1);
 while ((i!=0)&&(i!=3)){
 i=showmenu(&sta,&menu);
 }
 menufree(&sta,&menu);
-sta.music = Mix_LoadMUS("music/games.mp3");
+//sta.music = Mix_LoadMUS("music/games.mp3");
 Mix_PlayMusic(sta.music,-1);
 if (i==0){
 while (sta.run==1){
- dispose(&sta,&manj);
  Position_Update(&sta,&manj);
-if (collision (manj.pos1,manj.pos2)  ){
+Update_enemie (&enm,&manj);
+ dispose(&sta,&manj,&mach,&enm);
+
+//Machine_Position (&mach,&manj,&sta);
+if (sta.f!=NULL){
+Arduino_Update(&sta,&manj);
+}
+if (collision (manj.pos1,manj.pos2) ) {
 while (en.Num_try <4 && rep == 0){
 intialiser_en(&en);
 o=en.Num_try ;
 while (en.Num_try ==o && rep ==0){
 j= menigme(&en,&sta);
 rep =reponse(&en,j);
+Update_temp(&en,&sta,&rep);
 }
 }
 }
 }
+}
+if (sta.f!=NULL){
+fclose(sta.f);
 }
 freeenigme(&en) ;
 freesurface(&sta,&manj);
