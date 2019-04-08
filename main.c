cd92f90f17c70385ad28650c43d2156bc42f6b0e
blaisetappa237 <47978772+blaisetappa237@users.noreply.github.com>
2019-04-08 16:23:53 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,83 +1,255 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #include <SDL/SDL.h>
-#include <SDL/SDL_Image.h>
-#include <SDL/SDL_mixer.h>
-#include "MyWork.h"
-
-
-int main()
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include "fct.h"
+char lireCaractere()
 {
-
-char pause;
-ecran e;
-perso p;
-fond f;
-SDL_Event test_event;
-int done =1;
-int boucle_jeux=1;
-
-screen= SDL_SetVideoMode(1200,700,0,SDL_HWSURFACE | SDL_DOUBLEBUF);
-e.posecran.x=0;
-e.posecran.y=0;
-e.posecran.w=personnage->w;
-e.posecran.w=personnage->h;
-f.positionfond=SDL_LoadBMP("Forest.bmp");
-SDL_BlitSurface(f.positionfond, NULL, e.e, &e.posecran);
-SDL_Flip(e.e);
-
-******************************************************************************************************************************
-SDL_Init(SDL_INIT_EVERYTHING);
-TTF_Init();
-if( SDL_Init(SDL_INIT_EVERYTHING)!=0){
-printf("unabel to initialize SDL:%s\n",SDL_GetError());
-return (-1);
+    char caractere = 0;
+    caractere = getchar(); // On lit le premier caractère
+    caractere = toupper(caractere); // On met la lettre en majuscule si elle ne l'est pas déjà
+    // On lit les autres caractères mémorisés un à un jusqu'au \n
+    while (getchar() != '\n') ;
+    return caractere; // On retourne le premier caractère qu'on a lu
 }
-********************************************************************************************************************************
+int main(int argc, char* argv[])
+{
+    SDL_Init;
+    TTF_Init;
+    char string1[100];
+    char string2[11];
+    int nombre_mot=0;
+    int j;
+    SDL_Surface *ecran=NULL,*positioncar[20]={NULL},*texte = NULL, *fond = NULL,*Enigm=NULL,*pp=NULL,*nn=NULL;
+    SDL_Surface *message2=NULL;
+    char pas[11];
+    SDL_Rect position,post,choix,position_msg;
+    SDL_Event event;
+    TTF_Font *police = NULL;
+    TTF_Font *po=NULL;
+    nombre_mot=piocherMot(string1);
 
- imageFond=SDL_LoadBMP("image/Forest.bmp");
-   p.personnage=IMG_Load("orc.png") ;
- SDL_SetColorKey(p.personnage,SDL_SRCCOLORKEY,SDL_MapRGB(p.personnage->format,255,255,255));
-  SDL_Init(SDL_INIT_VIDEO);
-  e.e=SDL_SetvideoMode(800,600,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
-  SDL_WM_SetCaption("Deplacer un personnage",NULL);
+    for(j=0;j<nombre_mot-1;j++)
+    {
+    string2[j]=string1[j];
+    pas[j]='*';
+    }
+    string2[nombre_mot]='\0'; 
+     pas[nombre_mot]='\0'; 
+    SDL_Color couleurBlanche = {255, 255, 255};
+   	char let='&';
 
-
-  SDL_Event event;
-
-  f.positionfond.x=0;
-  f.positionfond.y=0;
-  p.posPer.x=400;
-  p.posPer.y=300;
-  **********************************************************************************
-  SDL_Rect camera;
-  camera.x=0;
-  camera.y=0;
-  camera.w=1180;
-  camera.h=600;
-
-*******************************************************************
-SDL_Quit();
-return 0 ;
+    int nombre_chance=nombre_mot+3;
+    char nombre = (char)nombre_chance;
+    int continuer = 1;
+    position.x=0;
+    position.y=0;
+    post.x=15;
+    post.y=15;
+    choix.x=30;
+    choix.y=30;
+    position_msg.x=10;
+    position_msg.y=105;
+  
+    SDL_Init(SDL_INIT_VIDEO);
+    ecran = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Afficher_Enigme", NULL);
+     TTF_Init();
+    
+    char lettre = 0; // Stocke la lettre proposée par l'utilisateur (retour du scanf)
+    char motSecret[100] = {0}; // Ce sera le mot à trouver
+    int *lettreTrouvee = NULL; // Un tableau de booléens. Chaque case correspond à une lettre du mot secret. 0 = lettre non trouvée, 1 = lettre trouvée
+    long coupsRestants = 10; // Compteur de coups restants (0 = mort)
+    long i = 0; // Une petite variable pour parcourir les tableaux
+    long tailleMot = 0;
+      fond = IMG_Load("moraira.jpg");
+     police = TTF_OpenFont("fente.ttf", 30);
+     texte = TTF_RenderText_Blended(police, "Bienvenue dans le pendu !", couleurBlanche);
+     
+     
+      fond = IMG_Load("moraira.jpg");
+     po = TTF_OpenFont("fente.ttf", 30);
+     Enigm = TTF_RenderText_Blended(po, " Quel est le mot secret\n", couleurBlanche);
+     nn = TTF_RenderText_Blended(po, " il vous reste\n", couleurBlanche);
+     
+      fond = IMG_Load("moraira.jpg");
+     po = TTF_OpenFont("fente.ttf", 30);
+     pp = TTF_RenderText_Blended(po, pas, couleurBlanche);
+     
+     char st[255];
+    sprintf(st,"il vous reste %d coups",nombre_chance);
+    message2=TTF_RenderText_Blended(po,st, couleurBlanche);
+     
+     
+     while (continuer)
+    {
+        while(SDL_PollEvent(&event))
+        switch(event.type)
+        {
+              
+    case SDL_QUIT:
+        continuer = 0;
+        break;
+    case SDL_MOUSEBUTTONUP: /* Clic de la souris */
+        break;
+        case SDL_KEYDOWN:
+                 
+    switch(event.key.keysym.sym)
+    {                      
+	case SDLK_a:
+	let='a';
+	break;
+	case SDLK_b:
+	let='b';
+	break;
+	case SDLK_c:
+	let='c';
+	break;
+	case SDLK_d:
+	let='d';
+	break;
+	case SDLK_e:
+	let='e';
+	break;
+	case SDLK_f:
+	let='f';
+	break;
+	case SDLK_g:
+	let='g';
+	break;
+	case SDLK_h:
+	let='h';
+	break;
+	case SDLK_i:
+	let='i';
+	break;
+	case SDLK_j:
+	let='j';
+	break;
+	case SDLK_k:
+	let='k';
+	break;
+	case SDLK_l:
+	let='l';
+	break;
+	case SDLK_m:
+	let='m';
+	break;
+	case SDLK_n:
+	let='n';
+	break;
+	case SDLK_o:
+	let='o';
+	break;
+	case SDLK_p:
+	let='p';
+	break;
+	case SDLK_q:
+	let='q';
+	break;
+	case SDLK_r:
+	let='r';
+	break;
+	case SDLK_s:
+	let='s';
+	break;
+	case SDLK_t:
+	let='t';
+	break;
+	case SDLK_u:
+	let='u';
+	break;
+	case SDLK_v:
+	let='v';
+	break;
+	case SDLK_y:
+	let='y';
+	break;
+	case SDLK_w:
+	let='w';
+	break;
+	case SDLK_x:
+	let='x';
+	break;
+	case SDLK_z:
+	let='z';
+	break;
+	case SDLK_BACKSPACE:
+	break;
+	default:
+	let='&';
+	break;
+        }
+        
+ if(nombre_chance>=0){
+             if(rechercheLettre(let,string2,nombre_mot)!=-1){
+          fprintf(stderr,"%c\n",let);
+         for(j=0;j<nombre_mot;j++){
+          
+             if(string2[j]==let){
+                 pas[j]=let;
+             }
+         }
+             SDL_FreeSurface(pp);
+         pp = TTF_RenderText_Blended(po, pas, couleurBlanche);
+         SDL_FreeSurface(message2);
+    sprintf(st,"il vous reste %d coups",nombre_chance);
+    message2=TTF_RenderText_Blended(po,st, couleurBlanche);
+     }
+     else{
+         nombre_chance--; 
+         SDL_FreeSurface(message2);
+    sprintf(st,"il vous reste %d coups",nombre_chance);
+    message2=TTF_RenderText_Blended(po,st, couleurBlanche);
+     }
+ }
+    break;     
+        }
+  
+  if(verifier_reponse(pas,string2,nombre_mot)==1 && nombre_chance>0){
+             SDL_FreeSurface(message2);
+    message2=TTF_RenderText_Blended(po,"vous avez gagne", couleurBlanche);
+  }
+  if(nombre_chance<=0){
+             SDL_FreeSurface(message2);
+             sprintf(st,"vous avez perdu  reponse : %s ",string2);
+    message2=TTF_RenderText_Blended(po,st, couleurBlanche);      
+  }
+        SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 0, 0, 0));
+        position.x = 0;
+        position.y = 0;
+        SDL_BlitSurface(fond, NULL, ecran, &position); /* Blit du fond */
+        position.x = 0;
+        position.y = 0;
+        SDL_BlitSurface(texte, NULL, ecran, &position); /* Blit du texte */ 
+        
+        post.x = 0;
+        post.y = 35;
+        SDL_BlitSurface(Enigm, NULL, ecran, &post); 
+        
+        
+        choix.x = 0;
+        choix.y = 70;
+        SDL_BlitSurface(pp, NULL, ecran, &choix); /* Blit du texte */
+        SDL_BlitSurface(message2,NULL,ecran,&position_msg);
+        
+         SDL_Flip(ecran);
+    }
+    
+    TTF_CloseFont(police);
+    TTF_CloseFont(po);
+    TTF_Quit();
+    SDL_FreeSurface(texte);
+    SDL_FreeSurface(Enigm);
+    SDL_Quit;
+    
+        
+        EXIT_SUCCESS;
+    
+          
+        
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
-}
--- makefile
+++ makefile
@@ -1,8 +1,6 @@
-
-prog:main.o gamefonc.o
-	gcc main.o gamefonc.o  -o prog -lSDL -lSDL_image  -lSDL_mixer -lSDL_ttf
+prog:main.o fct.o
+	gcc main.o fct.o -lSDL -lSDL_image -lSDL_ttf -o prog
 main.o:main.c
-	gcc main.c -c  -lSDL -lSDL_image -lSDL_mixer -lSDL_ttf
-gamefonc.o:gamefonc.c 
-	gcc gamefonc.c -c  -lSDL -lSDL_image -lSDL_mixer -lSDL_ttf
-
+	gcc main.c  -lSDL -lSDL_image -lSDL_ttf -c
+fct.o:fct.c
+	gcc -c fct.c
